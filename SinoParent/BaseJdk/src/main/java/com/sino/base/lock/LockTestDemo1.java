44b43549ce8cd98e4df033dd9405136aadc5099a
Lonny <springfor@163.com>
2018-07-27 09:50:08 +0800

    aaa

--- SinoParent/BaseJdk/src/main/java/com/sino/base/lock/LockTestDemo1.java
+++ SinoParent/BaseJdk/src/main/java/com/sino/base/lock/LockTestDemo1.java
@@ -1,116 +1,161 @@
 package com.sino.base.lock;
 
-import java.util.HashMap;
-
-
 /**
- * synchronized and  ReentrantLock
+ * synchronized and ReentrantLock
  *
  * @author lonny
  */
 public class LockTestDemo1 {
+	 Account  account = new Account();
+	 Account2 account2 = new Account2();
+	 Account3 account3 = new Account3();
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
+		LockTestDemo1 lock = new LockTestDemo1();
+		
+//		lock.withReentLock();
+		lock.withoutLock();
+//		lock.withSync();
+		lock.outPutBalance();
+	}
 
-        withReentLock();
-        HashMap<String, String> stringStringHashMap = new HashMap<>();
+	private void outPutBalance() {
+		while (true) {
+			System.out.println(account.getBalance());
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 
-//		withoutLock();
-//		withSync();
-    }
+	}
+	
+	
+	/**
+	 * 存钱
+	 */
+	private  void saveMoney() {
+		for (int i = 0; i < 100; i++) {
 
-    private  static void   saveMoney(){
-        for (int i = 0; i < 100; i++) {
-            Account3 account3 = new Account3();
-            account3.saveMoeny(Thread.currentThread().getName());
-            try {
-                Thread.sleep(50);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+			account.saveMoeny(Thread.currentThread().getName());
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
-    private static void withReentLock() {
+	/*
+	 * 取钱
+	 */
+	private  void getMoney() {
+		for (int i = 0; i < 100; i++) {
 
-        Thread thread = new Thread(LockTestDemo1::saveMoney);
-        thread.start();
+			account.getMoney(Thread.currentThread().getName());
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	/**
+	 * 存钱
+	 */
+	private  void saveMoney2() {
+		for (int i = 0; i < 100; i++) {
 
-        Thread thread2 = new Thread(LockTestDemo1::saveMoney);
-        thread2.start();
+			account2.saveMoeny(Thread.currentThread().getName());
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
+	/*
+	 * 取钱
+	 */
+	private  void getMoney2() {
+		for (int i = 0; i < 100; i++) {
 
-    }
+			account2.getMoney(Thread.currentThread().getName());
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	
+	/**
+	 * 存钱
+	 */
+	private  void saveMoney3() {
+		for (int i = 0; i < 100; i++) {
 
+			account3.saveMoeny(Thread.currentThread().getName());
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
-    public static void withSync() {
-        Account2 account2 = new Account2();
-        Runnable runnable = new Runnable() {
+	/*
+	 * 取钱
+	 */
+	private  void getMoney3() {
+		for (int i = 0; i < 100; i++) {
 
-            @Override
-            public void run() {
-                for (int i = 0; i < 100; i++) {
+			account3.getMoney(Thread.currentThread().getName());
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
-                    account2.saveMoeny(Thread.currentThread().getName());
-                    try {
-                        Thread.sleep(50);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
+	private  void withReentLock() {
 
-            }
-        };
+		Thread thread = new Thread(this::saveMoney3);
+		thread.start();
 
-        Runnable runnable2 = new Runnable() {
+		Thread thread2 = new Thread(this::getMoney3);
+		thread2.start();
 
-            @Override
-            public void run() {
+	}
 
+	/**
+	 * 同步
+	 */
+	public  void withSync() {
 
-            }
-        };
+		Thread thread = new Thread(this::saveMoney2);
+		thread.start();
 
-        new Thread(runnable2).start();
+		Thread thread2 = new Thread(this::getMoney2);
+		thread2.start();
+	}
 
-    }
+	/**
+	 * 不加锁
+	 */
+	public  void withoutLock() {
 
+		Thread thread = new Thread(this::saveMoney);
+		thread.start();
 
-    public static void withoutLock() {
+		Thread thread2 = new Thread(this::getMoney);
+		thread2.start();
 
-        Account account = new Account();
-        Runnable runnable = new Runnable() {
-
-            @Override
-            public void run() {
-                for (int i = 0; i < 100; i++) {
-                    account.saveMoeny(Thread.currentThread().getName());
-                }
-
-            }
-        };
-
-        Runnable runnable2 = new Runnable() {
-
-            @Override
-            public void run() {
-                for (int i = 0; i < 100; i++) {
-
-                    account.getMoney(Thread.currentThread().getName());
-                }
-
-            }
-        };
-
-        new Thread(runnable).start();
-        new Thread(runnable2).start();
-
-    }
-
-
-
-
-
-
+	}
 
 }
--- SinoParent/middleWare/src/test/java/com/sino/middle_ware/test/LockRegistryDemo.java
+++ SinoParent/middleWare/src/test/java/com/sino/middle_ware/test/LockRegistryDemo.java
@@ -4,7 +4,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Lock;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
